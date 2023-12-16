b14d3492e18458f77621f656ee2edda785feaa67
eitantal777 <eitantal777@gmail.com>
2023-12-16 12:50:26 -0700

    polygon test. see if lightburn will take it

--- boxgen.py
+++ boxgen.py
@@ -1,6 +1,14 @@
 import svgwrite
+from svgwrite import cm, mm
 
 dwg = svgwrite.Drawing('test.svg', profile='tiny')
-dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
-dwg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
+#dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
+#dwg.add(dwg.text('Test1', insert=(0, 0.2), fill='red'))
+
+part1 = dwg.add(dwg.g(id='part1', stroke='blue', fill='red'))
+#part1.add(dwg.rect(insert=(5*cm, 5*cm), size=(45*mm, 45*mm), stroke_width=3))
+points = [(0,10),(10,10),(20,11),(0,10),(0,0)]
+part1.add(dwg.polygon(points))
+#svgwrite.shapes.Polygon(points=[], **extra)
+
 dwg.save()
