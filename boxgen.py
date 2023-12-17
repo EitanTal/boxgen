0b6e146c31a8bbd75fb80dca8ab5c382b3bb1c4b
eitantal777 <eitantal777@gmail.com>
2023-12-16 20:51:26 -0700

    candidate version. Does not account for laser kerf

--- boxgen.py
+++ boxgen.py
@@ -21,21 +21,6 @@ dwg = svgwrite.Drawing('test.svg', profile='tiny')
 top = dwg.add(dwg.g(id='part1', stroke='blue', fill='red'))
 front = dwg.add(dwg.g(id='part1', stroke='blue', fill='red'))
 side = dwg.add(dwg.g(id='part1', stroke='blue', fill='red'))
-#part1.add(dwg.rect(insert=(5*cm, 5*cm), size=(45*mm, 45*mm), stroke_width=3))
-#points = [(330,207),(388,205),(390,189),(414,189),(413,300),(390,300),(390,280),(325,280)]
-#points = [(0*inch,0*inch),(box_width*inch,0*inch),(box_width*inch,box_depth*inch),(0*inch,box_depth*inch)]
-points = [(0,0),(box_width,0),(box_width,box_depth),(0,box_depth)]
-
-# scale does not affect translation:
-tmp = dwg.polygon(points)
-tmp.translate(7*inch,7*inch)
-tmp.scale(inch,inch)
-#top.add(tmp)
-
-# To add another, create a new polygon
-tmp = dwg.polygon(points)
-tmp.scale(inch,inch)
-#top.add(tmp)
 
 # Front panel:
 size_minus_overhang         = box_width - overhang - overhang - mat_thickness - mat_thickness
@@ -53,6 +38,53 @@ points = [(0,0),(top_tab,0),(top_tab,mat_thickness),
 tmp = dwg.polygon(points, stroke_width=0.1)
 tmp.scale(inch,inch)
 front.add(tmp)
+
+# Side panel:
+size_minus_overhang         = box_depth - overhang - overhang
+size_minus_overhang_andtab = size_minus_overhang - top_tab
+height_half_minus_tab       = (box_height - side_tab) / 2
+points = [(0,0),(top_tab,0),(top_tab,mat_thickness),
+          (size_minus_overhang_andtab, mat_thickness),(size_minus_overhang_andtab, 0),(size_minus_overhang,0),
+          (size_minus_overhang,height_half_minus_tab),(size_minus_overhang-mat_thickness,height_half_minus_tab),
+          (size_minus_overhang-mat_thickness,height_half_minus_tab+side_tab),(size_minus_overhang,height_half_minus_tab+side_tab),
+          (size_minus_overhang,box_height),(size_minus_overhang-top_tab,box_height),
+          (size_minus_overhang-top_tab,box_height-mat_thickness),
+          (top_tab,box_height-mat_thickness),(top_tab,box_height),(0,box_height),(0,height_half_minus_tab+side_tab),(mat_thickness,height_half_minus_tab+side_tab),
+          (mat_thickness,height_half_minus_tab),(0,height_half_minus_tab)]
+
+tmp = dwg.polygon(points, stroke_width=0.1)
+tmp.translate((box_width+1)*inch,0*inch)
+tmp.scale(inch,inch)
+side.add(tmp)
+
+# Top panel:
+points = [(0,0),(box_width,0),(box_width,box_depth),(0,box_depth)]
+tmp = dwg.polygon(points, stroke_width=0.1)
+tmp.translate((box_width+box_depth+2)*inch,0*inch)
+tmp.scale(inch,inch)
+top.add(tmp)
+# top panel - L's:
+points = [(0,0),(mat_thickness+top_tab,0),(mat_thickness+top_tab,mat_thickness),(mat_thickness,mat_thickness),(mat_thickness,top_tab),(0,top_tab)]
+tmp = dwg.polygon(points, stroke_width=0.1)
+tmp.translate(overhang*inch,overhang*inch)
+tmp.translate((box_width+box_depth+2)*inch,0*inch)
+tmp.scale(inch,inch)
+top.add(tmp)
+tmp = dwg.polygon(points, stroke_width=0.1)
+tmp.translate((box_width-overhang)*inch,overhang*inch)
+tmp.translate((box_width+box_depth+2)*inch,0*inch)
+tmp.scale(-inch,inch)
+top.add(tmp)
+tmp = dwg.polygon(points, stroke_width=0.1)
+tmp.translate((box_width-overhang)*inch,(box_depth-overhang)*inch)
+tmp.translate((box_width+box_depth+2)*inch,0*inch)
+tmp.scale(-inch,-inch)
+top.add(tmp)
+tmp = dwg.polygon(points, stroke_width=0.1)
+tmp.translate(overhang*inch,(box_depth-overhang)*inch)
+tmp.translate((box_width+box_depth+2)*inch,0*inch)
+tmp.scale(inch,-inch)
+top.add(tmp)
 
 
 dwg.save()
